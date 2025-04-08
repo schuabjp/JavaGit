fc4637889b0c8882e42986ddcc9162126bdd8bf4
schuabjp <joaoschuab@outlook.com>
2025-04-07 22:44:58 -0300

    Adiciona o conteúdo referente á 'Operadores Lógicos'

---
+++ aulas/src/QuartoTeste.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class QuartoTeste {
+    public static void main(String[] args) {
+        Scanner entradaCortisol = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade do seu Cortisol: ");
+        double cortisol = entradaCortisol.nextDouble();
+
+        //Cortisol ideal é de 6 a 18.4
+        boolean resultadoCortisol = cortisol >= 6.0 && cortisol <= 18.0;
+        System.out.printf("Cortisol: %b\n", resultadoCortisol);
+    }
+}
